329d9a392590dec2fe588645d818ec8e99c6ade7
Ailue <ailue@sina.cn>
2011-12-14 23:38:07 +0800

    宋氵エ

---
+++ lang/zh_CN/SPPackage.lua
@@ -0,0 +1,18 @@
+-- translation for SP Package
+
+return {
+	["sp"] = "SP",
+
+	["#_jiangsong"] = "呼保义",
+	["jiangsong"] = "宋江",
+	["yuzhong"] = "愚忠",
+	[":yuzhong"] = "你每杀死一名其他角色，可以令当前主公回复X点体力或摸X张牌；你每对当前主公造成一次伤害，可以令至多X名角色各摸一张牌或你摸两张牌，X为场上现存势力数",
+	["@yuzhong"] = "你可以发动【愚忠】，指定X名角色各摸一张牌，X为场上现存势力数",
+	["yuzhong:hp"] = "令当前主公回复X点体力",
+	["yuzhong:card"] = "令当前主公摸X张牌",
+	["yuzhong:all"] = "指定X名角色各摸一张牌",
+	["yuzhong:me"] = "自己摸两张牌",
+	["yuzhong:cancel"] = "不发动",
+	["shuntian"] = "顺天",
+	[":shuntian"] = "锁定技，你的势力视为当前主公的势力",
+}
--- src/package/sp-package.h
+++ src/package/sp-package.h
@@ -4,6 +4,17 @@
 #include "package.h"
 #include "card.h"
 
+class YuzhongCard: public SkillCard{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE YuzhongCard();
+
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual void onEffect(const CardEffectStruct &effect) const;
+    virtual bool targetsFeasible(const QList<const Player *> &targets, const Player *Self) const;
+};
+
 class SPPackage: public Package{
     Q_OBJECT
 
