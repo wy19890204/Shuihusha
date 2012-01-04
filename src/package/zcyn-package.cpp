b8cdbdb557f13897298fbe99e8c65c59155a54be
Ailue <ailue@sina.cn>
2012-01-04 11:14:33 +0800

    杨熊

--- src/package/zcyn-package.cpp
+++ src/package/zcyn-package.cpp
@@ -61,6 +61,41 @@ public:
     }
 };
 
+class Guizi: public TriggerSkill{
+public:
+    Guizi():TriggerSkill("guizi"){
+        events << Dying;
+    }
+
+    virtual bool triggerable(const ServerPlayer *target) const{
+        return !target->hasSkill(objectName());
+    }
+
+    virtual bool trigger(TriggerEvent, ServerPlayer *poolguy, QVariant &data) const{
+        Room *room = poolguy->getRoom();
+        ServerPlayer *bear = room->findPlayerBySkillName(objectName());
+        DyingStruct dying = data.value<DyingStruct>();
+        if(!bear || !dying.who)
+            return false;
+        if(dying.who == poolguy && room->askForCard(bear, "..S", "@guizi:" + poolguy->objectName(), data)){
+            room->playSkillEffect(objectName());
+            DamageStruct damage;
+            damage.from = bear;
+
+            LogMessage log;
+            log.type = "#Guizi";
+            log.from = bear;
+            log.to << poolguy;
+            log.arg = objectName();
+            room->sendLog(log);
+
+            room->killPlayer(poolguy, &damage);
+            return true;
+        }
+        return false;
+    }
+};
+
 SixiangCard::SixiangCard(){
 }
 
@@ -361,6 +396,9 @@ ZCYNPackage::ZCYNPackage()
 
     General *ruanxiaoer = new General(this, "ruanxiaoer", "min");
     ruanxiaoer->addSkill(new Fuji);
+
+    General *yangxiong = new General(this, "yangxiong", "jiang");
+    yangxiong->addSkill(new Guizi);
 
     General *haosiwen = new General(this, "haosiwen", "guan");
     haosiwen->addSkill(new Sixiang);
