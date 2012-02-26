ee3e0b9a90db58f4828f6c9fa747a246cd8a64b8
Ailue <ailue@sina.cn>
2012-02-26 16:24:46 +0800

    第一将

--- src/package/peasa.cpp
+++ src/package/peasa.cpp
@@ -6,9 +6,39 @@
 #include "engine.h"
 #include "ai.h"
 
+class Piaoyong: public PhaseChangeSkill{
+public:
+    Piaoyong():PhaseChangeSkill("piaoyong"){
+    }
+
+    virtual bool onPhaseChange(ServerPlayer *player) const{
+        if(player->getPhase() != Player::Start || !player->askForSkillInvoke(objectName()))
+            return false;
+        Room *room = player->getRoom();
+        player->skip(Player::Judge);
+        if(room->askForChoice(player, objectName(), "first+second") == "first")
+            player->skip(Player::Draw);
+        else
+            player->skip(Player::Play);
+        foreach(const Card *card, player->getJudgingArea())
+            room->throwCard(card);
+        ServerPlayer *target = room->askForPlayerChosen(player, room->getAllPlayers(), objectName());
+        Slash *slash = new Slash(Card::NoSuit, 0);
+        slash->setSkillName(objectName());
+        CardUseStruct use;
+        use.card = slash;
+        use.from = player;
+        use.to << target;
+        room->useCard(use, false);
+        return false;
+    }
+};
+
 PeasaPackage::PeasaPackage()
     :Package("peasa")
 {
+    General *guanzhang = new General(this, "guanzhang", "jiang");
+    guanzhang->addSkill(new Piaoyong);
 }
 
 ADD_PACKAGE(Peasa);
