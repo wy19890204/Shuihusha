47030928a03dce7de310bd62c2b84cac43780cda
Ailue <ailue@sina.cn>
2012-02-26 15:52:25 +0800

    建模

---
+++ src/package/peasa.cpp
@@ -0,0 +1,14 @@
+#include "standard.h"
+#include "skill.h"
+#include "peasa.h"
+#include "client.h"
+#include "carditem.h"
+#include "engine.h"
+#include "ai.h"
+
+PeasaPackage::PeasaPackage()
+    :Package("peasa")
+{
+}
+
+ADD_PACKAGE(Peasa);
---
+++ src/package/peasa.h
@@ -0,0 +1,14 @@
+#ifndef PEASAPACKAGE_H
+#define PEASAPACKAGE_H
+
+#include "package.h"
+#include "card.h"
+
+class PeasaPackage: public Package{
+    Q_OBJECT
+
+public:
+    PeasaPackage();
+};
+
+#endif // PEASAPACKAGE_H
