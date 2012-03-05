04e4e1fdb9600ef198778bd98d95025603a0dd67
Ailue <ailue@sina.cn>
2012-03-06 01:53:26 +0800

    更改包加载方式

--- src/core/card.h
+++ src/core/card.h
@@ -112,6 +112,7 @@ public:
     bool isOnce() const;
     bool isMute() const;
     bool willThrow() const;
+    bool canJilei() const;
 
     virtual void onUse(Room *room, const CardUseStruct &card_use) const;
     virtual void use(Room *room, ServerPlayer *source,  const QList<ServerPlayer *> &targets) const;
@@ -138,6 +139,7 @@ protected:
     QString skill_name;
     bool mute;
     bool will_throw;
+    bool can_jilei;
 
 private:
     Suit suit;
