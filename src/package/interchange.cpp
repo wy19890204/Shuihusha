3410b7b37de415e288bae90ea0fc38111a07a870
Ubun <ubun@foxmail.com>
2012-06-18 19:00:37 +0800

    MaxCardsSkill 影响技能：歃血、神算、吝啬

--- src/package/interchange.cpp
+++ src/package/interchange.cpp
@@ -176,6 +176,19 @@ public:
     }
 };
 
+class ShensuanMC: public MaxCardsSkill{
+public:
+    ShensuanMC():MaxCardsSkill("#shensuan-mc"){
+    }
+
+    virtual int getExtra(const Player *target) const{
+        if(target->hasSkill(objectName()))
+            return 2;
+        else
+            return 0;
+    }
+};
+
 class Gunzhu:public TriggerSkill{
 public:
     Gunzhu():TriggerSkill("gunzhu"){
@@ -668,6 +681,8 @@ InterChangePackage::InterChangePackage()
 
     General *shenjiangjing = new General(this, "shenjiangjing", "god", 3);
     shenjiangjing->addSkill(new Shensuan);
+    shenjiangjing->addSkill(new ShensuanMC);
+    related_skills.insertMulti("shensuan", "#shensuan-mc");
     shenjiangjing->addSkill(new Gunzhu);
 
     General *duwei = new General(this, "duwei", "jiang");
