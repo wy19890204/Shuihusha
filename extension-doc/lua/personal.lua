593cbd2c97c1049307e2ad481fa21d610fe3830a
Ailue <ailue@sina.cn>
2012-06-22 01:07:01 +0800

    finish skydao&noqing

--- extension-doc/lua/personal.lua
+++ extension-doc/lua/personal.lua
@@ -9,64 +9,69 @@ eatdeath=sgs.CreateTriggerSkill{
 	frequency = sgs.Skill_NotFrequent,
 	events={sgs.Death},
 
-	on_trigger=function(self,event,player,data)
+	can_trigger = function(self, player)
+		return true
+	end,
+
+	on_trigger = function(self,event,player,data)
         local room = player:getRoom()
 		local tenkei = room:findPlayerBySkillName(self:objectName())
-		if not tenkei then return false end
+		if not tenkei or event ~= sgs.Death then return false end
 
-        local eatdeath_skills = tenkei:getTag("EatDeath"):toList()
-		if(room:askForSkillInvoke(tenkei, self:objectName(), data)) then
+		local eatdeath_skills = tenkei:getTag("EatDeath"):toStringList()
+		if room:askForSkillInvoke(tenkei, self:objectName(), data) then
 			local eatdeaths = {}
 
-			for _, tmp in sgs.qlist(eatdeath_skills) do
-				table.insert(eatdeaths, tmp:toString())
-			    if not eatdeaths.isEmpty() then
-					local choice = room:askForChoice(tenkei, self:objectName(), eatdeaths.join("+"))
-					room:detachSkillFromPlayer(tenkei, choice)
-					for i = #eatdeath_skills, 1, -1 do
-						if eatdeath_skills[i]:objectName() == choice then
-							table.remove(eatdeath_skills, i)
-						end
-					end
+			if eatdeath_skills ~= nil then
+				eatdeath_skills = sgs.QList2Table(eatdeath_skills)
+				for _, tmp in ipairs(eatdeath_skills) do
+					table.insert(eatdeaths, tmp:toString())
 				end
-				room:loseMaxHp(tenkei)
-				local skills = player:getVisibleSkillList()
-				for _, skill in sgs.qlist(skills) do
-					if not (skill:isLordSkill() or player:isLord()) and skill:parent() then
-						local sk = skill:objectName()
-						room:acquireSkill(tenkei, sk)
-						table.insert(eatdeaths, sk)
-					end
-				end
-				local datatmp=sgs.QVariant(0)
-				datatmp:setValue(eatdeath_skills)
-				tenkei:setTag("EatDeath",datatmp)
 			end
+			if not eatdeaths.isEmpty() then
+				local choice = room:askForChoice(tenkei, self:objectName(), eatdeaths.join("+"))
+				room:detachSkillFromPlayer(tenkei, choice)
+				for i = #eatdeath_skills, 1, -1 do
+					if eatdeath_skills[i]:objectName() == choice then
+						table.remove(eatdeath_skills, i)
+					end
+				end
+			end
+			room:loseMaxHp(tenkei)
+			local skills = player:getVisibleSkillList()
+			for _, skill in sgs.qlist(skills) do
+			--	if skill:isLordSkill() and not player:isLord() then
+			--	elseif skill:parent() then
+				local sk = skill:objectName()
+				room:acquireSkill(tenkei, sk)
+				table.insert(eatdeath_skills, sk)
+			end
+			local datatmp = sgs.QVariant(0)
+			datatmp:setValue(eatdeath_skills)
+			tenkei:setTag("EatDeath", datatmp)
 		end
         return false
-	end,
+	end
 }
 
 skydao=sgs.CreateTriggerSkill
 {
 	name="skydao",
-	frequency =sgs.Skill_Compulsory,
+	frequency = sgs.Skill_Compulsory,
 	events={sgs.Damaged},
 
 	on_trigger=function(self,event,player,data)
-		local damage = data:toDamage()
         local room = player:getRoom()
-		if damage.to and damage.to == player and player:getPhase() == sgs.Player_NotActive then
-            local log=sgs.LogMessage()
+		if player:getPhase() == sgs.Player_NotActive then
+            local log = sgs.LogMessage()
 			log.type = "#SkydaoMAXHP"
 			log.from = player
-			log.arg = player:getMaxHP():toString()
+			log.arg = tonumber(player:getMaxHP())
 			log.arg2 = self:objectName()
-			room:setPlayerProperty(player, "maxhp", player:getMaxHP() + 1)
+			room:setPlayerProperty(player, "maxhp", sgs.QVariant(player:getMaxHP() + 1))
 			room:sendLog(log)
         end
-        return false
-	end,
+	end
 }
 
 noqing=sgs.CreateTriggerSkill{
@@ -77,31 +82,29 @@ noqing=sgs.CreateTriggerSkill{
 
 	on_trigger=function(self,event,player,data)
 		local room = player:getRoom()
-		local damage = data:toDamage()
-		if damage.to and damage.to == player then
-			for _, tmp in sgs.qlist(room:getOtherPlayers(player)) do
-				if tmp:getHp() < player:getHp() then
-					return false
-				end
-			end
-			for _, tmp in sgs.qlist(room:getAllPlayers()) do
-				local choice = room:askForChoice(tmp, objectName(), "hp+max_hp")
-				local log=sgs.LogMessage()
-				log.from = player
-				log.arg = self:objectName()
-				log.to:append(tmp)
-				if(choice == "hp") then
-					log.type = "#NoqingLoseHp"
-					room:sendLog(log)
-					room:loseHp(tmp)
-				else
-					log.type = "#NoqingLoseMaxHp"
-					room:sendLog(log)
-					room:loseMaxHp(tmp)
-				end
+		for _, tmp in sgs.qlist(room:getOtherPlayers(player)) do
+			if tmp:getHp() < player:getHp() then
+				return false
 			end
 		end
-	end,
+		for _, tmp in sgs.qlist(room:getAllPlayers()) do
+			local choice = room:askForChoice(tmp, self:objectName(), "hp+max_hp")
+			local log = sgs.LogMessage()
+			log.from = player
+			log.arg = self:objectName()
+			log.to:append(tmp)
+			if(choice == "hp") then
+				log.type = "#NoqingLoseHp"
+				room:sendLog(log)
+				room:loseHp(tmp)
+			else
+				log.type = "#NoqingLoseMaxHp"
+				room:sendLog(log)
+				room:loseMaxHp(tmp)
+			end
+		end
+		return false
+	end
 }
 
 tianqi:addSkill(eatdeath)
