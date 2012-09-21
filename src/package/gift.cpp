2c5f401f7febab6328fc9e9bd6013de576f45cb9
Ubun <ubun@foxmail.com>
2012-09-21 17:38:00 +0800

    我欲因之梦吴越，一夜飞度镜湖月

--- src/package/gift.cpp
+++ src/package/gift.cpp
@@ -59,17 +59,91 @@ public:
     }
 };
 
+Moonpie::Moonpie(Suit suit, int number):BasicCard(suit, number){
+    setObjectName("moonpie");
+}
+
+QString Moonpie::getSubtype() const{
+    return "gift_card";
+}
+
+QString Moonpie::getEffectPath(bool is_male) const{
+    return "audio/card/common/moonpie.ogg";
+}
+
+bool Moonpie::isAvailable(const Player *change) const{
+    return ServerInfo.GameMode == "dusong";
+}
+
+bool Moonpie::targetFilter(const QList<const Player *> &targets, const Player *houyi, const Player *change) const{
+    return targets.isEmpty() && change->getMark("HaveEaten2") == 0 && change->inMyAttackRange(houyi);
+}
+
+void Moonpie::onEffect(const CardEffectStruct &effect) const{
+    //room->throwCard(this);
+    Room *room = effect.from->getRoom();
+    // do animation
+    room->broadcastInvoke("animate", QString("moonpie:%1:%2")
+                          .arg(effect.from->objectName())
+                          .arg(effect.to->objectName()));
+
+    room->acquireSkill(effect.to, "beatjapan");
+    room->setPlayerMark(effect.to, "HaveEaten2", 1);
+}
+
+class BeatJapan: public ClientSkill{
+public:
+    BeatJapan():ClientSkill("beatjapan"){
+    }
+
+    virtual int getExtra(const Player *target) const{
+        if(!target->hasSkill(objectName()))
+            return 0;
+        else
+            return 2;
+    }
+};
+
+#include "maneuvering.h"
+class Beatjapan:public TriggerSkill{
+public:
+    Beatjapan():TriggerSkill("#beat-japan"){
+        events << CardUsed;
+    }
+
+    virtual bool trigger(TriggerEvent, Room* room, ServerPlayer *wusong, QVariant &data) const{
+        CardUseStruct use = data.value<CardUseStruct>();
+        if(use.card->inherits("TrickCard") && use.card->isRed()){
+            room->playSkillEffect(objectName());
+            LogMessage ogg;
+            ogg.type = "#BeatJapan";
+            ogg.from = wusong;
+            ogg.arg = objectName();
+
+            Card *card = new IronChain(use.card->getSuit(), use.card->getNumber());
+            card->addSubcard(use.card);
+            card->setSkillName(objectName());
+            CardUseStruct usechange = use;
+            usechange.card = card;
+            room->useCard(usechange);
+            return true;
+        }
+        return false;
+    }
+};
+
 GiftPackage::GiftPackage()
     :Package("gift")
 {
-    skills << new Lisao;
+    skills << new Lisao << new BeatJapan << Beatjapan;
+    related_skills.insertMulti("beatjapan", "#beat-japan");
     QList<Card *> cards;
 
     cards
             << new Zongzi(Card::Heart, 5)
             << new Zongzi(Card::Club, 5)
-            << new Zongzi(Card::Diamond, 5)
-            << new Zongzi(Card::Spade, 5)
+            << new Moonpie(Card::Diamond, 5)
+            << new Moonpie(Card::Spade, 5)
             ;
 
     foreach(Card *card, cards)
@@ -78,4 +152,4 @@ GiftPackage::GiftPackage()
     type = CardPack;
 }
 
-ADD_PACKAGE(Gift);
+ADD_PACKAGE(Gift)
