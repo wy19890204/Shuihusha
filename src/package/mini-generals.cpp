5d046b75bdfe66082b467e79b85fc890c1f72adb
ainue <ailue@sina.cn>
2012-11-06 01:25:59 +0800

    用猫做微博头像的内心都比较自闭 用狗做头像的都胸大 用小孩和卡通做头像的都长不大 用暗色系做头像的都内心阴暗 用文字做头像的都很二 用风景做头像的都是小清新 用自己做头像的，男的是基友，女的有公主病。

--- src/package/mini-generals.cpp
+++ src/package/mini-generals.cpp
@@ -257,7 +257,8 @@ bool PushouCard::targetFilter(const QList<const Player *> &targets, const Player
 }
 
 void PushouCard::onEffect(const CardEffectStruct &effect) const{
-    effect.from->playSkillEffect(skill_name, qrand() % 2 + 1);
+    int index = effect.from->getPhase() == Player::Start ? 1 : 3;
+    effect.from->playSkillEffect(skill_name, qrand() % 2 + index);
     effect.from->pindian(effect.to, skill_name, effect.card);
 }
 
@@ -321,10 +322,10 @@ public:
             if(pindian->from != hara && pindian->to != hara)
                 continue;
             if(pindian->isSuccess()){
-                room->playSkillEffect("pushou", 3);
+                room->playSkillEffect("pushou", pindian->from == hara ? 5 : 6);
                 pindian->from->drawCards(1);
             }else{
-                room->playSkillEffect("pushou", 4);
+                room->playSkillEffect("pushou", pindian->to == hara ? 5 : 6);
                 pindian->to->drawCards(1);
             }
         }
